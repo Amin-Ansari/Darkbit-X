628410db0f9cdcbf70133a3d1b436d3ca1887fc6
Amin Ansari <devansari@yahoo.com>
2023-05-03 11:07:48 +0330

    The texts are added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Fragment, useState } from "react";
 import Header from "./components/Header/Header";
+import TextSection from "./components/Body/TextSection";
+import FullWidthContainer from "./components/UI/FullWidthContainer";
 import "./App.css";
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <FullWidthContainer>
+        <TextSection />
+      </FullWidthContainer>
     </Fragment>
   );
 }
---
+++ src/components/Body/TextSection.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Container from "../UI/Container";
+import "./TextSection.css";
+
+const TextSection = () => {
+  return (
+    <Container className="gird-container">
+      <div className="context-wrapper">
+        <h3>راک بیت ایکس بروز ترین و فروش بورس و کریپتو کارنسی</h3>
+        <p>
+          لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
+          استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در
+          ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز
+        </p>
+      </div>
+    </Container>
+  );
+};
+
+export default TextSection;
