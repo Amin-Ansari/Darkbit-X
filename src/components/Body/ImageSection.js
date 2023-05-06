0db526c7026e04f2667b512f33828dc657260248
Amin Ansari <devansari@yahoo.com>
2023-05-06 10:01:22 +0330

    The section image is added

--- src/components/Body/ImageSection.js
+++ src/components/Body/ImageSection.js
@@ -2,13 +2,15 @@ import React from "react";
 import Container from "../UI/Container";
 import "./ImageSection.css";
 const gradientBG = require("../../assets/images/Untitled.png");
+const firstSectionImage = require("../../assets/images/first-section-big-image.png");
 
 const ImageSection = (props) => {
   return (
     <Container>
       <div className="image-section">
         <div className="gradient-image">
-          <img src={gradientBG} />
+          <img src={gradientBG} className="gradient-background" />
+          <img src={firstSectionImage} className="image-style" />
         </div>
       </div>
     </Container>
