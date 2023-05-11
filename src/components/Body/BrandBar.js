cc1b38eb284507df9df8bf6c7fd413e0255d5acc
Amin Ansari <devansari@yahoo.com>
2023-05-11 10:58:00 +0330

    The layout of the brand bar is done

--- src/components/Body/BrandBar.js
+++ src/components/Body/BrandBar.js
@@ -1,25 +1,27 @@
 import React from "react";
 import FullWidthContainer from "../UI/FullWidthContainer";
 import BrandImage from "./BrandImage";
+import useOnScreen from "../APIs/useOnScreen";
 import "./BrandBar.css";
 
 const BrandBar = (props) => {
+  const [brandRef, isBrandvisible] = useOnScreen({ threshold: 1.0 });
   return (
     <FullWidthContainer className="relative-wraper">
       <div className="shadow-box dark-box1"></div>
       <div className="shadow-box dark-box2"></div>
       <h4 className="brands-heading">برند های مطرح</h4>
-      <div className="brand-container">
+      <div
+        className={`brand-container ${isBrandvisible ? "bar-visible" : ""}`}
+        ref={brandRef}
+      >
         <BrandImage url="https://assets.website-files.com/623a72cac2795a8386c05faa/623ba71ab7e98c290218cee8_logo-1-darkbit-template.svg" />
+        <BrandImage url="https://assets.website-files.com/623a72cac2795a8386c05faa/623ba71ac5eb2ee132bb4c80_logo-6-darkbit-template.svg" />
+        <BrandImage url="https://assets.website-files.com/623a72cac2795a8386c05faa/623ba71aa23a6e4511f1922d_logo-4-darkbit-template.svg" />
+        <BrandImage url="https://assets.website-files.com/623a72cac2795a8386c05faa/623ba71b4159dae85bc529de_logo-3-darkbit-template.svg" />
+        <BrandImage url="https://assets.website-files.com/623a72cac2795a8386c05faa/623ba71ad5c47e7a413f61c8_logo-2-darkbit-template.svg" />
         <BrandImage url="https://assets.website-files.com/623a72cac2795a8386c05faa/623ba71ab7e98c290218cee8_logo-1-darkbit-template.svg" />
-        <BrandImage url="https://assets.website-files.com/623a72cac2795a8386c05faa/623ba71ab7e98c290218cee8_logo-1-darkbit-template.svg" />
-        <BrandImage url="https://assets.website-files.com/623a72cac2795a8386c05faa/623ba71ab7e98c290218cee8_logo-1-darkbit-template.svg" />
-        <BrandImage url="https://assets.website-files.com/623a72cac2795a8386c05faa/623ba71ab7e98c290218cee8_logo-1-darkbit-template.svg" />
-        <BrandImage url="https://assets.website-files.com/623a72cac2795a8386c05faa/623ba71ab7e98c290218cee8_logo-1-darkbit-template.svg" />
-        <BrandImage url="https://assets.website-files.com/623a72cac2795a8386c05faa/623ba71ab7e98c290218cee8_logo-1-darkbit-template.svg" />
-        <BrandImage url="https://assets.website-files.com/623a72cac2795a8386c05faa/623ba71ab7e98c290218cee8_logo-1-darkbit-template.svg" />
-        <BrandImage url="https://assets.website-files.com/623a72cac2795a8386c05faa/623ba71ab7e98c290218cee8_logo-1-darkbit-template.svg" />
-        <BrandImage url="https://assets.website-files.com/623a72cac2795a8386c05faa/623ba71ab7e98c290218cee8_logo-1-darkbit-template.svg" />
+        <BrandImage url="https://assets.website-files.com/623a72cac2795a8386c05faa/623ba71aa23a6e4511f1922d_logo-4-darkbit-template.svg" />
         <BrandImage url="https://assets.website-files.com/623a72cac2795a8386c05faa/623ba71ab7e98c290218cee8_logo-1-darkbit-template.svg" />
       </div>
     </FullWidthContainer>
