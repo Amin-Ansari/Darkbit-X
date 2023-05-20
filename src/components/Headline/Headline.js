cc02933386be47407a00d249e4d205dccc5c424a
Amin Ansari <devansari@yahoo.com>
2023-05-20 14:39:57 +0330

    Headline animation is done

--- src/components/Headline/Headline.js
+++ src/components/Headline/Headline.js
@@ -5,13 +5,22 @@ import useOnScreen from "../APIs/useOnScreen";
 import "./Headline.css";
 
 const Headline = (props) => {
+  const [headingRef, isHeadingVisible] = useOnScreen({ threshold: 1.0 });
+  const [pRef, isPVisible] = useOnScreen({ threshold: 1.0 });
   const { heading } = props;
   const { children } = props;
 
   return (
     <div className="headline">
-      <h3>{heading}</h3>
-      <p>{children}</p>
+      <h3
+        ref={headingRef}
+        className={`${isHeadingVisible ? "show-headline" : ""}`}
+      >
+        {heading}
+      </h3>
+      <p ref={pRef} className={`${isPVisible ? "show-headline" : ""}`}>
+        {children}
+      </p>
     </div>
   );
 };
