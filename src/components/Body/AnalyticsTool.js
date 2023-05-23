b3b0df696f90b5890403a49874242757e9c87af7
Amin Ansari <devansari@yahoo.com>
2023-05-23 12:40:25 +0330

    The AnalyticsTool component is done

--- src/components/Body/AnalyticsTool.js
+++ src/components/Body/AnalyticsTool.js
@@ -1,6 +1,15 @@
 import React from "react";
 import ToolSection from "./ToolSection";
 import Headline from "../Headline/Headline";
+import ToolItem from "./ToolItem";
+import { BsHandbag } from "react-icons/bs";
+import { HiOutlineDocumentReport } from "react-icons/hi";
+import { TbBrandGoogleAnalytics } from "react-icons/tb";
+import { BsFlag } from "react-icons/bs";
+import image1 from "../../assets/images/1.svg";
+import image2 from "../../assets/images/2.svg";
+import image3 from "../../assets/images/3.png";
+import image4 from "../../assets/images/4.svg";
 
 const AnalyticsTool = (props) => {
   return (
@@ -11,7 +20,22 @@ const AnalyticsTool = (props) => {
         سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای
         متنوع
       </Headline>
-      <ToolSection />
+      <ToolSection>
+        <ToolItem headingTitle="مانیتورینگ فعالیت ها" imageSource={image1}>
+          <BsHandbag />
+        </ToolItem>
+        <ToolItem headingTitle="گزارش های زنده" imageSource={image2}>
+          <HiOutlineDocumentReport />
+        </ToolItem>
+      </ToolSection>
+      <ToolSection>
+        <ToolItem headingTitle="آنالیز کابران" imageSource={image3}>
+          <TbBrandGoogleAnalytics />
+        </ToolItem>
+        <ToolItem headingTitle="مشخصات کاربران" imageSource={image4}>
+          <BsFlag />
+        </ToolItem>
+      </ToolSection>
     </React.Fragment>
   );
 };
--- src/components/Body/ToolSection.js
+++ src/components/Body/ToolSection.js
@@ -1,21 +1,8 @@
 import React from "react";
-import ToolItem from "./ToolItem";
-import { BsHandbag } from "react-icons/bs";
-// import { IconName } from "react-icons/bs";
-// import { IconName } from "react-icons/bs";
-// import { IconName } from "react-icons/bs";
 import "./ToolSection.css";
 
-import image1 from "../../assets/images/1.svg";
-
 const ToolSection = (props) => {
-  return (
-    <div className="tool-container">
-      <ToolItem headingTitle="مانیتورینگ فعالیت ها" imageSource={image1}>
-        <BsHandbag />
-      </ToolItem>
-    </div>
-  );
+  return <div className="tool-container">{props.children}</div>;
 };
 
 export default ToolSection;
