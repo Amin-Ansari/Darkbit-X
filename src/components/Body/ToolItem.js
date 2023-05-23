4127abe50a466a1b426365682f112ca9cb1d01db
Amin Ansari <devansari@yahoo.com>
2023-05-23 12:20:43 +0330

    The ToolItem Component is done

--- src/components/Body/ToolItem.js
+++ src/components/Body/ToolItem.js
@@ -1,13 +1,23 @@
 import React from "react";
 import ToolHeading from "./ToolHeading";
 import ToolList from "./ToolList";
+import useOnScreen from "../APIs/useOnScreen";
 import "./ToolItem.css";
 
 const ToolItem = (props) => {
+  const [itemRef, itemVisibility] = useOnScreen({ threshold: 0.7 });
   return (
-    <div className="tool-item">
-      <ToolHeading title={props.headingTitle}>{props.children}</ToolHeading>
-      <ToolList />
+    <div
+      className={`tool-item ${itemVisibility ? "item-appear" : ""}`}
+      ref={itemRef}
+    >
+      <div className="tool-context">
+        <ToolHeading title={props.headingTitle}>{props.children}</ToolHeading>
+        <ToolList />
+      </div>
+      <div className="tool-image-wrapper">
+        <img src={props.imageSource} />
+      </div>
     </div>
   );
 };
--- src/components/Body/ToolSection.js
+++ src/components/Body/ToolSection.js
@@ -6,10 +6,12 @@ import { BsHandbag } from "react-icons/bs";
 // import { IconName } from "react-icons/bs";
 import "./ToolSection.css";
 
+import image1 from "../../assets/images/1.svg";
+
 const ToolSection = (props) => {
   return (
     <div className="tool-container">
-      <ToolItem headingTitle="مانیتورینگ فعالیت ها">
+      <ToolItem headingTitle="مانیتورینگ فعالیت ها" imageSource={image1}>
         <BsHandbag />
       </ToolItem>
     </div>
