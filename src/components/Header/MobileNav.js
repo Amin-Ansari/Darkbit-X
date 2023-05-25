bdb9739d9f32cf21a8b736626f6faab4a4c02ca6
Amin Ansari <devansari@yahoo.com>
2023-05-25 18:06:46 +0430

    The bug of mobileNav got fixed

--- src/components/Header/MobileNav.js
+++ src/components/Header/MobileNav.js
@@ -13,35 +13,37 @@ const MobileNav = (props) => {
   };
 
   return (
-    <div className={`mobile-nav ${props.className}`}>
-      <ul>
-        <NavLink className="mobile-nav-link">
-          <span>خانه</span>
-        </NavLink>
-        <NavLink className="mobile-nav-link">
-          <span>درباره ما</span>
-        </NavLink>
-        <NavLink className="mobile-nav-link">
-          <span>بلاگ</span>
-        </NavLink>
-        <NavLink onToggling={toggleTheSubMenu}>
-          <span>منابع</span>
-          <span className={`angle-icon ${submenuState ? "rotate-icon" : ""}`}>
-            <FiChevronDown />
-          </span>
-        </NavLink>
-        {submenuState && (
-          <div className="mobile-mega">
-            <ul>
-              <NavLink className="mobile-nav-link">دوره ها</NavLink>
-              <NavLink className="mobile-nav-link">تغییرات</NavLink>
-              <NavLink className="mobile-nav-link">پنل کاربری</NavLink>
-              <NavLink className="mobile-nav-link">بوت کمپ ها</NavLink>
-            </ul>
-          </div>
-        )}
-      </ul>
-      <GradientButton className="mobile-gB">شروع خرید</GradientButton>
+    <div className="mobile-nav-container">
+      <div className={`mobile-nav ${props.className}`}>
+        <ul>
+          <NavLink className="mobile-nav-link">
+            <span>خانه</span>
+          </NavLink>
+          <NavLink className="mobile-nav-link">
+            <span>درباره ما</span>
+          </NavLink>
+          <NavLink className="mobile-nav-link">
+            <span>بلاگ</span>
+          </NavLink>
+          <NavLink onToggling={toggleTheSubMenu}>
+            <span>منابع</span>
+            <span className={`angle-icon ${submenuState ? "rotate-icon" : ""}`}>
+              <FiChevronDown />
+            </span>
+          </NavLink>
+          {submenuState && (
+            <div className="mobile-mega">
+              <ul>
+                <NavLink className="mobile-nav-link">دوره ها</NavLink>
+                <NavLink className="mobile-nav-link">تغییرات</NavLink>
+                <NavLink className="mobile-nav-link">پنل کاربری</NavLink>
+                <NavLink className="mobile-nav-link">بوت کمپ ها</NavLink>
+              </ul>
+            </div>
+          )}
+        </ul>
+        <GradientButton className="mobile-gB">شروع خرید</GradientButton>
+      </div>
     </div>
   );
 };
