73f121960610ecc488a720316e1fcaf8498f68bd
Amin Ansari <devansari@yahoo.com>
2023-05-29 14:27:44 +0330

    Moving the slider is done

--- src/components/Body/FeedCartContainer.js
+++ src/components/Body/FeedCartContainer.js
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import FeedBackCart from "./FeedBackCart";
 import "./FeedCartContainer.css";
 import GradientButton from "../UI/GradientButton";
+import { TbUserExclamation } from "react-icons/tb";
+import { FaLaptopHouse } from "react-icons/fa";
 const avatar1 = require("../../assets/images/Avatarts/1.jpg");
 const avatar2 = require("../../assets/images/Avatarts/2.jpg");
 const avatar3 = require("../../assets/images/Avatarts/3.jpg");
 const avatar4 = require("../../assets/images/Avatarts/1.jpg");
 
 const FeedCartContainer = (props) => {
+  const [rightValue, setrightValue] = useState(0);
+
+  const myRef = useRef();
+  const previous = () => {
+    const slideWidth = myRef.current.firstChild.clientWidth;
+    setrightValue((rightValue) => rightValue - slideWidth);
+  };
+  const next = () => {
+    const slideWidth = myRef.current.firstChild.clientWidth;
+    setrightValue((rightValue) => rightValue + slideWidth);
+  };
   return (
     <div className="cart-container">
       <div className="shadow-box"></div>
-      <div className="feed-carts">
+      <div
+        className="feed-carts"
+        ref={myRef}
+        style={{ right: `${rightValue}px` }}
+      >
         <FeedBackCart
           imageSource={avatar1}
           qoute="بهترین ابزار آنالیز"
@@ -30,8 +47,12 @@ const FeedCartContainer = (props) => {
         ></FeedBackCart>
       </div>
       <div className="button-holder">
-        <button className="slider-button">P</button>
-        <button className="slider-button">N</button>
+        <button className="slider-button" onClick={next}>
+          P
+        </button>
+        <button className="slider-button" onClick={previous}>
+          N
+        </button>
       </div>
     </div>
   );
