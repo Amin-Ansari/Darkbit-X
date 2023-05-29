2a888260f338a815dca86da3bab0c85c7ec8122e
Amin Ansari <devansari@yahoo.com>
2023-05-29 13:49:42 +0330

    Buttons are added

--- src/components/Body/FeedCartContainer.js
+++ src/components/Body/FeedCartContainer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FeedBackCart from "./FeedBackCart";
 import "./FeedCartContainer.css";
+import GradientButton from "../UI/GradientButton";
 const avatar1 = require("../../assets/images/Avatarts/1.jpg");
 const avatar2 = require("../../assets/images/Avatarts/2.jpg");
 const avatar3 = require("../../assets/images/Avatarts/3.jpg");
@@ -10,16 +11,28 @@ const FeedCartContainer = (props) => {
   return (
     <div className="cart-container">
       <div className="shadow-box"></div>
-      <FeedBackCart
-        imageSource={avatar1}
-        qoute="بهترین ابزار آنالیز"
-      ></FeedBackCart>
-      <FeedBackCart imageSource={avatar2} qoute="سریعترین ابزار"></FeedBackCart>
-      <FeedBackCart
-        imageSource={avatar3}
-        qoute="مدرن ترین ابزار"
-      ></FeedBackCart>
-      <FeedBackCart imageSource={avatar4} qoute="بسیا پرکاربرد"></FeedBackCart>
+      <div className="feed-carts">
+        <FeedBackCart
+          imageSource={avatar1}
+          qoute="بهترین ابزار آنالیز"
+        ></FeedBackCart>
+        <FeedBackCart
+          imageSource={avatar2}
+          qoute="سریعترین ابزار"
+        ></FeedBackCart>
+        <FeedBackCart
+          imageSource={avatar3}
+          qoute="مدرن ترین ابزار"
+        ></FeedBackCart>
+        <FeedBackCart
+          imageSource={avatar4}
+          qoute="بسیا پرکاربرد"
+        ></FeedBackCart>
+      </div>
+      <div className="button-holder">
+        <button className="slider-button">P</button>
+        <button className="slider-button">N</button>
+      </div>
     </div>
   );
 };
